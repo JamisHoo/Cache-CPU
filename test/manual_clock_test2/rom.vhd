575810732a222981a0de8288d679bff7afbfb68e
李天润 <ltrthu@163.com>
2014-12-08 22:46:41 +0800

    add rom.vhd

--- test/manual_clock_test2/rom.vhd
+++ test/manual_clock_test2/rom.vhd
@@ -3,27 +3,33 @@ use IEEE.STD_LOGIC_1164.all;
 
 package rom is
 
-constant ROM_SIZE : integer := 17;
+constant ROM_SIZE : integer := 22;
 TYPE ROM is array(0 to ROM_SIZE - 1) of std_logic_vector(31 downto 0);
 
 constant boot_rom : ROM := (
-        X"24160001",
-        X"0016b7c0",
-        X"2415003f",
-        X"aed50000",
-        X"8ed40000",
-        X"26d60004",
-        X"26b50001",
-        X"aed50000",
-        X"8ed40000",
-        X"26d60004",
-        X"26b50001",
-        X"aed50000",
-        X"8ed40000",
-        X"26d60004",
-        X"26b50001",
-        X"aed50000",
-        X"8ed40000"
+X"241000be",
+X"00108600",
+X"8e110000",
+X"8e120004",
+X"00129400",
+X"02328825",
+X"26100008",
+X"24120008",
+X"00129700",
+X"8e130000",
+X"8e140004",
+X"0014a400",
+X"02749825",
+X"ae530000",
+X"26520004",
+X"26100008",
+X"2631ffff",
+X"1e20fff7",
+X"bf000000",
+X"bf000000",
+X"1210fffe",
+X"bf000000"
+
     ); 
 
 end rom;
